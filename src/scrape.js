89c8bf426e405041e1d8f1f0f908fb03d42a00e5
Ka'vi <kavi.orca@gmail.com>
2018-03-22 17:14:37 -0500

    Adding an early return if the character selector is going to time out.  Instead should inspect the not found page and return from that

--- src/scrape.js
+++ src/scrape.js
@@ -6,42 +6,46 @@ async function fetchCharacter(character) {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
 
-    let response = null;
+    let result = null;
 
-    await page.goto(`https://www.f-list.net/c/${character}`, {waitUntil: 'networkidle2'});
-    await page.click('#SplashWarningYes');
-    await page.waitForSelector('#Character_FetishList');
+    try {
+        await page.goto(`https://www.f-list.net/c/${character}`, {waitUntil: 'networkidle2'});
+        await page.click('#SplashWarningYes');
+        await page.waitForSelector('#Character_FetishList', {timeout: 5000});
 
-    const tabs = await page.$$('#tabs ul li a');
-    const imageCount = await page.evaluate(() => {
-        return parseInt(
-            document
-            .querySelector('a[href="#tabs-4"]')
-            .textContent
-            .match(/\((\d*)\)/)[1]
-        );
-    });
+        const tabs = await page.$$('#tabs ul li a');
+        const imageCount = await page.evaluate(() => {
+            return parseInt(
+                document
+                .querySelector('a[href="#tabs-4"]')
+                .textContent
+                .match(/\((\d*)\)/)[1]
+            );
+        });
 
-    // Images tab
-    if (imageCount > 0) {
-        await tabs[3].click();
-        try {
-            await page.waitForSelector('#tabs-4 div a', {timeout: 2000});
-        } catch (err) {
-            // TODO: Do we want to do something here?
+        // Images tab
+        if (imageCount > 0) {
+            await tabs[3].click();
+            try {
+                await page.waitForSelector('#tabs-4 div a', {timeout: 2000});
+            } catch (err) {
+                // TODO: Do we want to do something here?
+            }
         }
+
+        await page.addScriptTag({path: path.join(__dirname, 'inject.js')});
+
+        result = await page.evaluate(async () => {
+            return {
+                tabs: fetchTabs(),
+                kinks: fetchKinkLists(),
+            };
+        });
+    } catch (err) {
+        throw err;
+    } finally {
+        await browser.close();
     }
-
-    await page.addScriptTag({path: path.join(__dirname, 'inject.js')});
-
-    const result = await page.evaluate(async () => {
-        return {
-            tabs: fetchTabs(),
-            kinks: fetchKinkLists(),
-        };
-    });
-
-    await browser.close();
 
     return result;
 }
--- test/index.js
+++ test/index.js
@@ -6,12 +6,26 @@ const {fetchCharacter} = require('../src/scrape');
 describe('f-list.net tests', async function () {
 
     it('Should fetch a character', async function () {
-        this.timeout(5000);
+        this.timeout(8000);
         const result = await fetchCharacter('ka-vi');
 
         expect(result).to.have.all.keys('tabs', 'kinks');
         expect(result.kinks).to.have.all.keys('Fave', 'Yes', 'Maybe', 'No');
         expect(result.tabs).to.have.all.keys('info', 'details', 'images');
+    });
+
+    it('Should exit early if a character is not found', async function () {
+        this.timeout(8000);
+
+        let error = null;
+
+        try {
+            await fetchCharacter('blarglenope');
+        } catch (err) {
+            error = err;
+        }
+
+        expect(error).to.not.be.null;
     });
 
 });
